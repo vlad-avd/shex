9762440ff1d3b62c96ead103a913c91aa3b74972
Vladyslav Avdiienko <avdienko.vladyslav@gmail.com>
2022-11-09 01:13:44 +0200

    init tauri system-tray app skeleton

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+use tauri::{AppHandle, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, Wry};
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let click = CustomMenuItem::new("click".to_string(), "Click here");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_item(click);
+
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(handle_tray_event)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+fn handle_tray_event(app: &AppHandle<Wry>, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+                "quit" => app.exit(0),
+                item => {println!("Clicked on: {item}")}
+            }
+        }
+        _ => {}
+    }
+}
